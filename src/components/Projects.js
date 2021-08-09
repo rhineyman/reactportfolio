0c478a90c99a786b471dbc7ac1141cffa38e25db
Mike Rhiney <rhineyman@gmail.com>
2021-08-09 09:16:50 -0700

    Title change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
         <div className="px-2">
             <h2>Day Planner</h2>
             <p>This is a simple calender designed to plan out your day.</p>
-            <p>linked deployment:<a href="https://rhineyman.github.io/Calender/" target="_blank">Making Space</a> </p>
+            <p>linked deployment:<a href="https://rhineyman.github.io/Calender/" target="_blank">Day Planner</a> </p>
             <p>github link: <a href="https://github.com/rhineyman/Calender" target="_blank">Github</a></p>
         </div>
         </section>
