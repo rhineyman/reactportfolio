57cc7ba53c818e6151c9b0cd69617e3510ce0219
Mike Rhiney <rhineyman@gmail.com>
2021-10-12 11:43:19 -0700

    about me change

--- src/components/About.js
+++ src/components/About.js
@@ -15,17 +15,9 @@ function About() {
 			</div>
 			<div>
 				<p>
-					My name is Mike Rhiney and I am a former service industry worker making a transition to the tech
-					field. Before joining the University of Oregon full stack coding program, I attended the University of Alabama
-					where I studied buisness management. After leaving Alabama I moved to Whitefish Montana where I
-					worked for a variety of Buisnesses including a fine dining restaurant, craft distillery, and a
-					composting company. Over the 6 years I spent in Montana I held almost every possible position in the restaraunt world, both front and back of house. 
-					In 2018 my partner made the decision to get her masters	degree in Portland. In Portland I became the Food and Beverage Supervisor at
-					the Waterfront Marriot. This position was responsible for the scheduling, purchasing and flow of the
-					4 restaraunts in the building. Before covid I transitioned to a new purchasing role as the Beverage
-					Controller. While in that position I became more proficient in excel and the Fintech
-					purchasing software. I recently completed the full stack coding program at the University of Oregon and
-					am excited to start a new career!
+					My name is Mike Rhiney and I am a former service industry worker currently transitioning into the tech field in Portland, OR.  I recently completed the University of Oregon full stack coding program.  I am originally from Tuscaloosa, Alabama and I attended the University of Alabama where I studied business management.  I left Alabama to pursue my passion for skiing in Whitefish Montana, where I worked for a variety of local businesses.  Throughout the years spent in Montana I developed a wide range of skills working in fine dining, a craft distillery, and a composting company.  In 2018 my partner and I relocated to Portland, and I became the
+					Food and Beverage Supervisor at the Waterfront Marriott.  This position was responsible for the scheduling, purchasing and flow of the four restaurants in the building. Before Covid I transitioned to a new purchasing role as the Beverage Controller, where I became more proficient in excel and Fintech purchasing software.  When I lost this position due to the pandemic I decided to pursue web development and coding, as computer languages and gaming have long been interests and hobbies of mine.  I completed the full stack coding program at the University of Oregon and am eager to start a new career in web development!
+
 				</p>
 			</div>
 		</section>
